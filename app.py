70ff81200ebde143f0046bb32d8b29e9129da3b4
chLynda <ch.lynda2019@gmail.com>
2025-02-13 22:33:12 +0100

    Depot Formulaire Secu

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file, render_template
 from flask_cors import CORS
 import sqlite3
 import hashlib
@@ -6,7 +6,9 @@ import os
 
 app = Flask(__name__)
 CORS(app)
-
+@app.route('/')
+def home():
+    return render_template('index.html')
 # Configuration
 UPLOAD_FOLDER = 'static/images'
 if not os.path.exists(UPLOAD_FOLDER):
